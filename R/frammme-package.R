3b9bdbc61c318ef95ccda2242025d8313e359a31
bdshaff <52110434+bdshaff@users.noreply.github.com>
2020-05-07 21:59:53 -0400

    package documentation
    add package documentation

---
+++ R/frammme-package.R
@@ -0,0 +1,11 @@
+#' frammme: Framework for MMM projects
+#'
+#' Functions to Generate an MMM Project Framework.
+#' The main function of this package is `create_mmm_project`
+#' which takes parameters as arguments and creates a folder with an RStudio project following the MMM modeling framework.
+#'
+#' @section Functions:
+#'
+#' @docType package
+#' @name framme
+NULL
