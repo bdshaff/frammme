0c2b68f99e06e646eab72a5b3af2f3ff1566b0fe
bdshaff <52110434+bdshaff@users.noreply.github.com>
2020-05-12 11:59:42 -0400

    documentation
    documentation

--- R/frammme-package.R
+++ R/frammme-package.R
@@ -6,6 +6,26 @@
 #'
 #' @section Functions:
 #'
+#' @usage
+#' This example creates an MMM modeling project directory at the location pointed to by the path argument.
+#' The add_drake_workflow adds a subdirectory with a drake workflow set up for execution.
+#'
+#' library(frammme)
+#' create_mmm_project(path = "~/Desktop/",
+#'                    FiscalYear = "FY19",
+#'                    modeling_start_date = "2015-04-01",
+#'                    modeling_end_date = "2019-09-30",
+#'                    add_drake_workflow = TRUE,
+#'                    open_proj = TRUE,
+#'                    init_packrat = FALSE)
+#'
+#' @details The result of the call to create_mmm_project creates a directory with the following directory tree.
+#' Model scripts are automatically added and ready for execution via the helper functions create_submodel_script,
+#' create_salesmodel_script, and create_responsecurve_script.
+#'
+#' You can run fs::dir_tree() to see the structure of the created project.
+#'
+#'
 #' @docType package
 #' @name frammme
 NULL
