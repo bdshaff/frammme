8f3d1018dfc258e56c46e5cdc5652237d80bf345
bdshaff <52110434+bdshaff@users.noreply.github.com>
2020-07-22 18:18:11 -0400

    edits
    edits

--- R/create_mmm_project.R
+++ R/create_mmm_project.R
@@ -1,141 +1,145 @@
-#' Creat an MMM Project
+#' Create an MMM Project
 #'
 #' @param path - path to location where project folder should be created.
 #' @param FiscalYear - string specifying the fiscal period for the project.
-#' @param modeling_start_date - yyyy-mm-dd fromat string.
-#' @param modeling_end_date - yyyy-mm-dd fromat string.
+#' @param modeling_start_date - yyyy-mm-dd format string.
+#' @param modeling_end_date - yyyy-mm-dd format string.
 #' @param add_drake_workflow - logical. should a drake workflow subdirectory be created.
 #' @param open_proj - logical. should the Rstudio project be opened when created.
 #' @param init_packrat - logical. should a packrat folder be initiated automatically.
 #'
 
-create_mmm_project = function(path = "~/Desktop/", FiscalYear = NULL,
-                              modeling_start_date = NULL, modeling_end_date = NULL,
-                              add_drake_workflow = TRUE,
-                              open_proj = TRUE,
-                              init_packrat = FALSE){
-  require(drake)
-  require(tidyverse)
-  require(usethis)
-  require(here)
-  require(OneStepBrandScience)
-  require(decompR)
-  require(frammme)
-  require(mmmlegacy)
-  require(mmmactive)
+create_mmm_project <- function(path = "~/Desktop/", FiscalYear = NULL,
+                               modeling_start_date = NULL, modeling_end_date = NULL,
+                               add_drake_workflow = TRUE,
+                               open_proj = TRUE,
+                               init_packrat = FALSE) {
+  library(drake)
+  library(tidyverse)
+  library(usethis)
+  library(here)
+  library(OneStepBrandScience)
+  library(decompR)
+  library(frammme)
+  library(mmmlegacy)
+  library(mmmactive)
 
-  if(!dir.exists(path)){
-    stop(stringr::str_c(path," doesn't exist. Please specify a valid path to directory."))
+  if (!dir.exists(path)) {
+    stop(stringr::str_c(path, " doesn't exist. Please specify a valid path to directory."))
   }
-  #1 Check the format of the start and end date strings
-  #2 Check that fiscal year is a valid string
+  # 1 Check the format of the start and end date strings
+  # 2 Check that fiscal year is a valid string
 
-  mmm_proj_name = stringr::str_c(FiscalYear,"-Modeling")
-  dir_loc = stringr::str_c(path,"/",mmm_proj_name)
+  mmm_proj_name <- stringr::str_c(FiscalYear, "-Modeling")
+  dir_loc <- stringr::str_c(path, "/", mmm_proj_name)
   print(dir_loc)
 
-  if(dir.exists(dir_loc)){
-    stop(str_c("Project at ",dir_loc," already exists."))
-  }else{
+  if (dir.exists(dir_loc)) {
+    stop(str_c("Project at ", dir_loc, " already exists."))
+  } else {
     dir.create(dir_loc)
     setwd(dir_loc)
   }
 
   usethis::create_project(path = dir_loc, open = open_proj, rstudio = TRUE)
-  if(init_packrat == TRUE){
+  if (init_packrat == TRUE) {
     packrat::init()
   }
 
-  if(!dir.exists("modeling")){
+  if (!dir.exists("modeling")) {
     dir.create("modeling")
 
-    dir.create("modeling/ModelStrings")
+    #dir.create("modeling/ModelStrings")
 
     dir.create("modeling/SubModels")
-    sbms = c("KBA","MA","OAO","PC","PI","SRC")
-    for(sbm in sbms){
-      location = stringr::str_c("modeling/SubModels/",sbm)
+    sbms <- c("KBA", "MA", "OAO", "PC", "PI", "SRC")
+    for (sbm in sbms) {
+      location <- stringr::str_c("modeling/SubModels/", sbm)
       dir.create(location)
-      dir.create(stringr::str_c(location,"/", FiscalYear,"-AllModels"))
+      dir.create(stringr::str_c(location, "/", FiscalYear, "-AllModels"))
       print(location)
-      print(str_c(location,"/", FiscalYear,"-AllModels"))
-      create_submodel_script(SBM = sbm, NMP = "ALL",
-                             FiscalYear = FiscalYear,
-                             modeling_start_date = modeling_start_date,
-                             modeling_end_date = modeling_end_date)
+      print(str_c(location, "/", FiscalYear, "-AllModels"))
+      create_submodel_script(
+        SBM = sbm, NMP = "ALL",
+        FiscalYear = FiscalYear,
+        modeling_start_date = modeling_start_date,
+        modeling_end_date = modeling_end_date
+      )
     }
 
     dir.create("modeling/SalesModels")
-    nmps = c("ALT","ARM","FRO","LEF","MAX","MUR","NV","PTH","RGE","RGS","SEN","TTN","VER")
-    for(nmp in nmps){
-      location = stringr::str_c("modeling/SalesModels/",nmp)
+    nmps <- c("ALT", "ARM", "FRO", "LEF", "MAX", "MUR", "NV", "PTH", "RGE", "RGS", "SEN", "TTN", "VER")
+    for (nmp in nmps) {
+      # location <- stringr::str_c("modeling/SalesModels/", nmp)
+      # print(location)
+      # dir.create(location)
+      # create_salesmodel_script(
+      #   NMP = nmp, FiscalYear = FiscalYear,
+      #   modeling_start_date = modeling_start_date,
+      #   modeling_end_date = modeling_end_date
+      # )
+
+      location <- stringr::str_c("modeling/SalesModels/", nmp, "-Bayes")
       print(location)
       dir.create(location)
-      create_salesmodel_script(NMP = nmp, FiscalYear = FiscalYear,
-                               modeling_start_date = modeling_start_date,
-                               modeling_end_date = modeling_end_date)
-
-      location = stringr::str_c("modeling/SalesModels/",nmp,"-Bayes")
-      print(location)
-      dir.create(location)
-      create_salesmodelbayes_script(NMP = nmp, FiscalYear = FiscalYear,
-                                    modeling_start_date = modeling_start_date,
-                                    modeling_end_date = modeling_end_date)
-
+      create_salesmodelbayes_script(
+        NMP = nmp, FiscalYear = FiscalYear,
+        modeling_start_date = modeling_start_date,
+        modeling_end_date = modeling_end_date
+      )
     }
 
-    dir.create("modeling/ResponseCurves")
-    for(nmp in nmps){
-      location = stringr::str_c("modeling/ResponseCurves/",nmp)
-      print(location)
-      dir.create(location)
-      create_responsecurve_script(NMP = nmp, FiscalYear = FiscalYear,
-                                  modeling_start_date = modeling_start_date,
-                                  modeling_end_date = modeling_end_date)
-
-    }
-
+    # dir.create("modeling/ResponseCurves")
+    # for (nmp in nmps) {
+    #   location <- stringr::str_c("modeling/ResponseCurves/", nmp)
+    #   print(location)
+    #   dir.create(location)
+    #   create_responsecurve_script(
+    #     NMP = nmp, FiscalYear = FiscalYear,
+    #     modeling_start_date = modeling_start_date,
+    #     modeling_end_date = modeling_end_date
+    #   )
+    # }
   }
 
-  if(!dir.exists("output")){
+  if (!dir.exists("output")) {
     dir.create("output")
 
     dir.create("output/SubModels")
-    sbms = c("KBA","MA","OAO","PC","PI","SRC")
-    for(sbm in sbms){
-      dir.create(stringr::str_c("output/SubModels/",sbm))
+    sbms <- c("KBA", "MA", "OAO", "PC", "PI", "SRC")
+    for (sbm in sbms) {
+      dir.create(stringr::str_c("output/SubModels/", sbm))
     }
 
     dir.create("output/SalesModels")
-    dir.create("output/ResponseCurves")
-    nmps = c("ALT","ARM","FRO","LEF","MAX","MUR","NV","PTH","RGE","RGS","SEN","TTN","VER")
-    for(nmp in nmps){
-      dir.create(stringr::str_c("output/SalesModels/",nmp))
-      print(stringr::str_c("output/SalesModels/",nmp))
-      dir.create(stringr::str_c("output/SalesModels/",nmp,"-Bayes"))
-      print(stringr::str_c("output/SalesModels/",nmp,"-Bayes"))
-      dir.create(stringr::str_c("output/ResponseCurves/",nmp))
-      print(stringr::str_c("output/ResponseCurves/",nmp))
+    #dir.create("output/ResponseCurves")
+    nmps <- c("ALT", "ARM", "FRO", "LEF", "MAX", "MUR", "NV", "PTH", "RGE", "RGS", "SEN", "TTN", "VER")
+    for (nmp in nmps) {
+      #dir.create(stringr::str_c("output/SalesModels/", nmp))
+      #print(stringr::str_c("output/SalesModels/", nmp))
+      dir.create(stringr::str_c("output/SalesModels/", nmp, "-Bayes"))
+      print(stringr::str_c("output/SalesModels/", nmp, "-Bayes"))
+      #dir.create(stringr::str_c("output/ResponseCurves/", nmp))
+      #print(stringr::str_c("output/ResponseCurves/", nmp))
     }
-
   }
 
-  if(!dir.exists("data")){
+  if (!dir.exists("data")) {
     dir.create("data")
     dir.create("data/processed_data")
     dir.create("data/categorization")
     dir.create("data/fit_curves")
   }
 
-  if(!dir.exists("R")){
+  if (!dir.exists("R")) {
     dir.create("R")
   }
 
-  if(dir.exists("R")){
+  if (dir.exists("R") & FALSE) { #disabling this for now
     print("R/model_strings.R")
     file.create("R/model_strings.R")
-    script = '
-require(stringr)
+    script <- '
+library(stringr)
 ######################################################################
 ############################ SRC Submodel ############################
 ######################################################################
@@ -625,8 +629,7 @@ saveRDS(stringformatPanel_Div, "modeling/ModelStrings/ver-model-string.Rds")
     source("R/model_strings.R")
   }
 
-  if(add_drake_workflow == TRUE){
+  if (add_drake_workflow == TRUE) {
     add_drake_workflow(FiscalYear = FiscalYear)
   }
-
 }
--- R/create_salesmodelbayes_script.R
+++ R/create_salesmodelbayes_script.R
@@ -16,45 +16,142 @@ create_salesmodelbayes_script = function(NMP = NULL, FiscalYear = NULL, modeling
          \n  FiscalPeriod = \"', FiscalYear, '\"\n')
 
   script_body = '
-input_file_ModelData = here::here("data","processed_data","ModelData.xlsx")
-input_file_ModelFitCurves = here::here("data","fit_curves","Fit_Curves.xlsx")
+input_file_ModelFitCurves <- here::here("data", "fit_curves", "Fit_Curves.xlsx")
+input_file_ModelData <- here::here("data", "processed_data", "ModelData.xlsx")
+input_file_FMIData <- here::here("data", "processed_data", "FMI.csv")
+input_file_SpendData <- here::here("data", "roi_app_data", "daily_spend_data.csv")
+input_file_MSRPData <- here::here("data/roi_app_data/MSRP.csv")
 
-input_file_ModelSetup = here::here("modeling","SalesModels",str_c(NMP,"-Bayes"),"input",str_c(nmp,"-ModelSetup.csv"))
-input_file_ModelSpec = here::here("modeling","SalesModels",str_c(NMP,"-Bayes"),"input",str_c(nmp,"-ModelSpec.csv"))
+input_files <-
+  list(
+    input_file_ModelData = input_file_ModelData,
+    input_file_FMIData = input_file_FMIData,
+    input_file_SpendData = input_file_SpendData,
+    input_file_MSRPData = input_file_MSRPData
+  )
 
-out_dest_mod_obj = here::here("output","SalesModels",str_c(NMP,"-Bayes"),str_c(NAMEPLATE,"-",FiscalPeriod,"-mod_obj.Rds"))
+input_file_ModelSetup <- here::here("modeling", "SalesModels", str_c(NMP, "-Bayes"), "input", str_c(nmp, "-ModelSetup.csv"))
+input_file_ModelSpec <- here::here("modeling", "SalesModels", str_c(NMP, "-Bayes"), "input", str_c(nmp, "-ModelSpec2.csv"))
+
+out_dest_mod_obj <- here::here("output", "SalesModels", str_c(NMP, "-Bayes"), str_c(NAMEPLATE, "-", FiscalPeriod, "-mod_obj.Rds"))
+
+##############################################################
+# Define the Dummy Variable Function
+##############################################################
+
+Add_Dummy <- function(mod_obj) {
+  cs <- mod_obj$cs
+  ts <- mod_obj$Time
+
+  n_cols_before <- ncol(mod_obj$data)
+
+  mod_obj$data <-
+    mod_obj$data %>%
+    mutate(
+      DUM_2017_11_01 = if_else(!!sym(ts) == "2017-11-01", 1, 0),
+      DUM_2018_01_01 = if_else(!!sym(ts) == "2018-01-01", 1, 0),
+      DUM_2017_05_01 = if_else(!!sym(ts) == "2017-05-01", 1, 0),
+      DUM_2018_03_01 = if_else(!!sym(ts) == "2018-03-01", 1, 0),
+      DUM_2018_09_01 = if_else(!!sym(ts) == "2018-09-01", 1, 0)
+    )
+
+  n_added_cols <- ncol(mod_obj$data) - n_cols_before
+  n_cols_after <- ncol(mod_obj$data)
+  added_dummy_cols <- names(mod_obj$data)[(n_cols_after - (n_added_cols - 1)):n_cols_after]
+
+  dummy_spec <-
+    data.frame(added_dummy_cols, added_dummy_cols, "Base", "Dummy", 1, "None", 0, 1) %>%
+    set_colnames(names(mod_obj$spec)[c(1:6, 17:18)])
+
+  mod_obj$spec <-
+    bind_rows(mod_obj$spec, dummy_spec)
+
+  return(mod_obj)
+}
 
 ##############################################################
 ########################### START ############################
 ##############################################################
 
-# line by line
-mod_obj = create_mod_obj()
-mod_obj = add_group_selector(mod_obj, vehicles = nmp, regions = rgn)
-mod_obj = activate_model_setup(mod_obj, input_file_ModelSetup)
-mod_obj = activate_model_spec(mod_obj, input_file_ModelSpec)
-mod_obj = Load_FitCurves(mod_obj, input_file_ModelFitCurves)
-mod_obj = Load_Data(mod_obj, input_file_ModelData)
-mod_obj = Transform(mod_obj, print = FALSE)
-mod_obj = SetDateRange(mod_obj)
-mod_obj = Run_Model(mod_obj)
-
-# or tidy-style
-mod_obj =
-  create_mod_obj() %>%
-  add_group_selector(vehicles = nmp, regions = rgn) %>%
-  activate_model_setup(input_file_ModelSetup) %>%
-  activate_model_spec(input_file_ModelSpec) %>%
-  Load_FitCurves(input_file_ModelFitCurves) %>%
-  Load_Data(input_file_ModelData) %>%
-  Transform(print = FALSE) %>%
-  SetDateRange() %>%
-  Run_Model()
-
-Plot_Resids(mod_obj, panel_name = "all")
+mod_obj <- create_mod_obj()
+mod_obj <- add_group_selector(mod_obj, vehicles = nmp, regions = rgn)
+mod_obj <- activate_model_setup(mod_obj, input_file_ModelSetup)
+mod_obj <- activate_model_spec(mod_obj, input_file_ModelSpec)
+mod_obj <- Load_FitCurves(mod_obj, input_file_ModelFitCurves)
+mod_obj <- Load_Data(mod_obj, input_files)
+mod_obj <- Transform(mod_obj, print = TRUE)
+mod_obj <- Set_Date_Range(mod_obj)
+mod_obj <- Add_Dummy(mod_obj)
+mod_obj <- Run_Model_Panel(mod_obj)
+mod_obj <- Decomp(mod_obj)
 
 ##############################################################
-################## EXPORT AND SAVE RESULTS ###################
+################ LOAD SUBMODELS AND UNNEST ###################
+##############################################################
+
+submodels <- mmm_load_submodels(mod_obj$nmp)
+unnested_decomp <- mmm_unnest_models(NMP = mod_obj$NMP, salesmodel = mod_obj$decomp_list, submodels = submodels)
+mod_obj$unnested_decomp_list <- unnested_decomp
+
+##############################################################
+########################### RC ###############################
+##############################################################
+
+Ctable <-
+  mod_obj$unnested_decomp_list$varCont %>%
+  filter(Categories != "Date") %>%
+  pivot_longer(names_to = "Date", values_to = "Value", 5:ncol(.)) %>%
+  mutate(
+    Date = ymd(Date),
+    FiscalYear = FY(Date, FYBegin = 4)
+  ) %>%
+  group_by(variable, Categories, Model, Group, FiscalYear) %>%
+  summarise(Value = sum(Value, na.rm = TRUE)) %>%
+  pivot_wider(names_from = FiscalYear, values_from = Value) %>%
+  ungroup() %>%
+  filter(!(grepl("PANEL", variable) & !grepl(mod_obj$NMP, variable))) %>%
+  mutate(Categories = case_when(
+    Categories %in% c("TV T1", "TV T1 MBR") ~ "TV T1",
+    Categories %in% c("TV T2 EVENT", "TV T2 PRODUCT") ~ "TV T2",
+    Categories %in% c("DIGITAL TOTAL T1", "DIGITAL TOTAL T1 MBR", "DIGITAL T1") ~ "Digital Display T1",
+    Categories %in% c("DIGITAL TOTAL T2 MBR", "DIGITAL TOTAL T2", "DIGITAL T2") ~ "Digital T2",
+    Categories %in% c("STREAMING T1", "STREAMING T1 MBR") ~ "Streaming T1",
+    Categories %in% c("SEARCH") ~ "Search T1",
+    Categories %in% c("RADIO T2") ~ "Radio T2",
+    Categories %in% c("ADDRESSABLE TV T1") ~ "Addressable TV T1",
+    Categories %in% c("SOCIAL DISPLAY T1", "SOCIAL VIDEO T1") ~ "Social T1",
+    Categories %in% c("SOCIAL DISPLAY T2", "SOCIAL DISPLAY T2 MBR") ~ "Social T2",
+    Categories %in% c("MAGAZINE T1") ~ "Print",
+    TRUE ~ Categories
+  ))
+
+ABClist <-
+  ABCsReachGen(
+    Contrib = Ctable,
+    fmi_data = mod_obj$Ftable,
+    spend = mod_obj$Stable,
+    multiplier = pull(filter(mod_obj$Mtable, FiscalPeriod == "FY18"), MSRP),
+    fit = fit,
+    fit_displayt1 = fit_displayt1,
+    fit_displayt2 = fit_displayt2,
+    fit_st = fit_st,
+    fit_addressable = fit_addressable,
+    panel = mod_obj$nmp,
+    year = "2019",
+    From = as.character(mod_obj$EndDate - 721),
+    To = as.character(mod_obj$EndDate)
+  )
+
+mod_obj$ABClist <- ABClist
+
+##############################################################
+################## Media Mix Optimization ####################
+##############################################################
+
+mod_obj <- Media_Mix(mod_obj, total_budget = 81213085, incremental = 100000, interactive = FALSE)
+
+##############################################################
+############### EXPORT AND SAVE MODEL OBJECT #################
 ##############################################################
 
 saveRDS(mod_obj, out_dest_mod_obj)
