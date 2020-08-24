540dee523018e74f35e98c48f917abf0688c07fe
bdshaff <52110434+bdshaff@users.noreply.github.com>
2020-08-24 12:16:22 -0400

    Update mmm_load_submodels.R
    change KBA to WEB

--- R/mmm_load_submodels.R
+++ R/mmm_load_submodels.R
@@ -1,17 +1,17 @@
 mmm_load_submodels = function(nmp){
-  
+
   PI = mmm_load_submodel(nmp = nmp, submodel = "PI")
-  KBA = mmm_load_submodel(nmp = nmp, submodel = "KBA")
+  WEB = mmm_load_submodel(nmp = nmp, submodel = "WEB")
   PC = mmm_load_submodel(nmp = nmp, submodel = "PC")
   OAO = mmm_load_submodel(nmp = nmp, submodel = "OAO")
   SRC = mmm_load_submodel(nmp = nmp, submodel = "SRC")
   MA = mmm_load_submodel(nmp = nmp, submodel = "MA")
-  
+
   return(list(PI = PI,
-              KBA = KBA,
+              WEB = WEB,
               PC = PC,
               OAO = OAO,
               SRC = SRC,
               MA = MA))
-  
+
 }
